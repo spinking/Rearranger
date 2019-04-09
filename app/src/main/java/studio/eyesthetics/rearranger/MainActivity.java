52372ab121caa3101e4cbcdc6fd4d28d0abb90e5
Spinking <spinking@mail.ru>
2019-04-09 12:53:08 +0400

    add ViewModel

--- app/src/main/java/studio/eyesthetics/rearranger/MainActivity.java
+++ app/src/main/java/studio/eyesthetics/rearranger/MainActivity.java
@@ -1,12 +1,11 @@
 package studio.eyesthetics.rearranger;
 
-import android.content.Context;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ProgressBar;
@@ -17,22 +16,26 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewer;
     private UnderCover under;
     private String content;
-
-    MyTask mt;
+    public MyView myView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myView = ViewModelProviders.of(this).get(MyView.class);
+
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         progressBar.getIndeterminateDrawable().setColorFilter(0xFFAA3939, android.graphics.PorterDuff.Mode.MULTIPLY);
         edit = (EditText) findViewById(R.id.editText);
+        edit.setText(myView.getEditField());
         textViewer = (TextView) findViewById(R.id.textView3);
+        textViewer.setText(myView.getTextField());
         content = "";
         under = new UnderCover(this);
     }
 
-    class MyTask extends AsyncTask<Void, Void, Void> {
+     class MyTask extends AsyncTask<Void, Void, Void> {
         @Override
         protected  void onPreExecute() {
             super.onPreExecute();
@@ -54,11 +57,13 @@ public class MainActivity extends AppCompatActivity {
 
             if(under.res.size() != 0) {
                 for(String s : under.res) {
-                    textViewer.append(s + "\n");
+                    myView.setTextField(s);
                 }
             } else {
                 textViewer.append("No words");
             }
+
+            textViewer.setText(myView.getTextField());
 
             under.res.clear();
             under.set.clear();
@@ -67,17 +72,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view) {
         content = edit.getText().toString();
+        myView.setEditField(content);
 
-        mt = (MyTask) getLastCustomNonConfigurationInstance();
+        MyTask myTask = new MyTask();
+        myTask.execute();
 
-        if(mt == null) {
-            mt = new MyTask();
-            mt.execute();
-        }
-
-    }
-
-    public Object onRetainCustomNonConfigurationInstance() {
-        return mt;
     }
 }
---
+++ app/src/main/java/studio/eyesthetics/rearranger/MyView.java
@@ -0,0 +1,30 @@
+package studio.eyesthetics.rearranger;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MyView extends ViewModel {
+
+    private String editField = "";
+    private String textField = "";
+
+    public MyView(String editField, String textField) {
+        this.editField = editField;
+        this.textField = textField;
+    }
+    public MyView() {
+
+    }
+
+    public String getEditField() {
+        return editField;
+    }
+    public void setEditField(String editText) {
+        this.editField = editText;
+    }
+    public String getTextField() {
+        return textField;
+    }
+    public void setTextField(String textView) {
+        this.textField += textView + "\n";
+    }
+}
