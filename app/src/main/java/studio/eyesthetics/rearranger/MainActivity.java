d08807c4bd72bf8267e44c2e4d92eb3e740defe1
Spinking <spinking@mail.ru>
2019-04-09 09:11:46 +0400

    add some code

--- app/src/main/java/studio/eyesthetics/rearranger/MainActivity.java
+++ app/src/main/java/studio/eyesthetics/rearranger/MainActivity.java
@@ -1,13 +1,72 @@
 package studio.eyesthetics.rearranger;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private EditText edit;
+    private TextView textViewer;
+    private UnderCover under;
+    private String content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.getIndeterminateDrawable().setColorFilter(0xFFAA3939, android.graphics.PorterDuff.Mode.MULTIPLY);
+        edit = (EditText) findViewById(R.id.editText);
+        textViewer = (TextView) findViewById(R.id.textView3);
+        content = "";
+        under = new UnderCover(this);
+    }
+
+    class MyTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected  void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(ProgressBar.VISIBLE);
+            textViewer.setText("");
+        }
+        @Override
+        protected  Void doInBackground(Void... params) {
+            under.rearrange("", content);
+            under.returnWord();
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            textViewer.setMovementMethod(new ScrollingMovementMethod());
+            progressBar.setVisibility(ProgressBar.INVISIBLE);
+
+            if(under.res.size() != 0) {
+                for(String s : under.res) {
+                    textViewer.append(s + "\n");
+                }
+            } else {
+                textViewer.append("No words");
+            }
+
+            under.res.clear();
+            under.set.clear();
+        }
+    }
+
+    public void onClick(View view) {
+        content = edit.getText().toString();
+        MyTask myTask = new MyTask();
+        myTask.execute();
+
     }
 }
---
+++ app/src/main/java/studio/eyesthetics/rearranger/UnderCover.java
@@ -0,0 +1,66 @@
+package studio.eyesthetics.rearranger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.widget.Toast;
+
+public class UnderCover extends IOException {
+
+
+    Context context;
+    UnderCover(Context context) {
+        this.context = context;
+    }
+    Set<String> set = new HashSet<>();
+    Set<String> res = new HashSet<>();
+
+    public void rearrange (String prefix, String str){
+        set.add(prefix);
+        for (char ch : str.toCharArray()) {
+            rearrange(prefix + ch, str.replaceFirst(ch + "", ""));
+
+        }
+    }
+
+    public void returnWord() {
+
+        InputStream fileInputStream = null;
+
+        try{
+            AssetManager assetManager = context.getAssets();
+            fileInputStream = assetManager.open("vocabulary.txt");
+
+            byte[] textContent = new byte[fileInputStream.available()];
+            fileInputStream.read(textContent);
+            String[] lines = new String(textContent).split("\n");
+            Set<String> hashLines = new HashSet<String>(Arrays.asList(lines));
+
+            for(String str : set) {
+                if(hashLines.contains(str)) {
+                    this.res.add(str);
+                }
+            }
+
+        } catch(Exception a){
+            //Toast.makeText(this, a.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        finally{
+            try{
+                if(fileInputStream!= null) {
+                    fileInputStream.close();
+                    //this.res.clear();
+                    //set.clear();
+                }
+            } catch(IOException ex) {
+                //Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
